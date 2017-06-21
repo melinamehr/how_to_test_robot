32d72b4ae5b37465dc9bbc41962a56cc684d30f6
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-21 16:52:29 -0400

    tests 1-4 complete

--- test_robot.rb
+++ test_robot.rb
@@ -4,64 +4,42 @@ require './robot.rb'
 
 class TestRobot < MiniTest::Test
 
-  def test_that_foreign_vintage_robot_needing_repairs_sent_to_station_1
-    skip
-    # arrange
-    
-    # act
-
-    # assert
-  end
-
-  def test_that_foreign_robot_neeing_repairs_sent_to_station_1
-    skip
-    # arrange
-    
-    # act
-
-    # assert
+  def test_that_foreign_robot_needing_repairs_sent_to_station_1
+    robot = Robot.new
+      robot.foreign_model = true
+      robot.needs_repairs = true
+      assert_equal 1, robot.station
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
-    # arrange
-    
-    # act
-
-    # assert
+    robot = Robot.new
+    robot.vintage_model = true
+    robot.needs_repairs = true
+    assert_equal 2, robot.station
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
-    # arrange
-    
-    # act
-
-    # assert
+    robot = Robot.new
+    robot.needs_repairs = true
+    assert_equal 3, robot.station
   end
-  
+
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
-    # arrange
-    
-    # act
-
-    # assert
+    robot = Robot.new
+    assert_equal 4, robot.station
   end
-  
-  def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
-    # arrange
-    
-    # act
 
-    # assert
+  def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
+    robot = Robot.new
+    robot.todos = 0
+    assert_equal -1, robot.tasks
+
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
     skip
     # arrange
-    
+
     # act
 
     # assert
@@ -70,7 +48,7 @@ class TestRobot < MiniTest::Test
   def test_workday_on_day_off_returns_true
     skip
     # arrange
-    
+
     # act
 
     # assert
@@ -79,7 +57,7 @@ class TestRobot < MiniTest::Test
   def test_workday_not_day_off_returns_false
     skip
     # arrange
-    
+
     # act
 
     # assert
