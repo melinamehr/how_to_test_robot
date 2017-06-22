5d0d953a490d76365d000aeccfdfff0ff94d796f
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-22 11:48:12 -0400

    completed questions and fixed syntax

--- test_robot.rb
+++ test_robot.rb
@@ -6,9 +6,10 @@ class TestRobot < MiniTest::Test
 
   def test_that_foreign_robot_needing_repairs_sent_to_station_1
     robot = Robot.new
-      robot.foreign_model = true
-      robot.needs_repairs = true
+    robot.foreign_model = true
+    robot.needs_repairs = true
       assert_equal 1, robot.station
+      #assert @robot.station == 1
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
@@ -31,36 +32,27 @@ class TestRobot < MiniTest::Test
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
     robot = Robot.new
-    robot.todos = 0
-    assert_equal -1, robot.tasks
-
+    robot.todos = []
+    assert_equal -1, robot.prioritize_tasks
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
-    # arrange
-
-    # act
-
-    # assert
+    robot = Robot.new
+    robot.todos = [2,3,1]
+    assert robot.prioritize_tasks == 3
   end
 
-  def test_workday_on_day_off_returns_true
-    skip
-    # arrange
-
-    # act
-
-    # assert
+  def test_workday_on_day_off_returns_false
+    robot = Robot.new
+    robot.day_off = 2
+    refute robot.workday?(2)
   end
 
-  def test_workday_not_day_off_returns_false
-    skip
-    # arrange
+  def test_workday_not_day_off_returns_true
+    robot = Robot.new
+    robot.day_off = "Monday"
+    assert robot.workday?("Tuesday")
 
-    # act
-
-    # assert
   end
 
 end
